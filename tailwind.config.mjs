e437e3531d410028dfa74522b5b5a06ee966036c
jramma <jrmasip097@gmail.com>
2025-01-24 17:56:51 +0100

    responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -23,6 +23,9 @@ export default {
         pyellow: "#F2CF96",
         ppurlple: "#9D859A",
         pblack: "#4E6273",
+        white:{
+          DEFAULT:"#F2F2F2",
+        }
       },
       fontFamily: {
         mono: ["Alexandria", "monospace"],
