0e6fa02cd890033482ee9869f645dbde871c84f6
jramma <jrmasip097@gmail.com>
2025-01-24 13:02:10 +0100

    hero.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
 });
